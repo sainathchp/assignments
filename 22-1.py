e424370139ea1189b02852054069273b1ac8d96d
sainathchp <53533119+sainathchp@users.noreply.github.com>
2021-02-23 12:39:39 +0530

    Update 22-1.py

--- 22-1.py
+++ 22-1.py
@@ -1,14 +1,35 @@
 s=input()
+a=list(map(int,input().strip().split()))
+s1=list(map(str,input().strip().split()))
+s2=list(map(str,input().strip().split()))
 l1=[]
 l2=[]
-c=0
-for i in range(len(s)):
-    if(s[i].isdigit()==True):
-        l1.append(s[i])
-for i in range(0,10):
-    for j in range(len(l1)):
-        if(int(i)==int(l1[j])):
-            c+=1
-    l2.append(c)
-    c=0
-print(*l2)
+l3=[]
+l=list(s)
+l4=[]
+p=''
+for i in range(len(s1)):
+    l1.append(len(s1[i]))
+for i in range(len(a)):
+    k=a[i]+l1[i]
+    for j in range(a[i],k):
+        p+=s[j]
+    l2.append(p)
+    p=''
+for i in range(len(l2)):
+    if(s1[i]==l2[i]):
+        k=a[i]+l1[i]
+        for j in range(a[i],k):
+            if(j>=len(s)):
+                break
+            else:
+                l3.append(l[j])
+    else:
+        continue
+
+res = [i for i in l if i not in l3]
+d=list(res)
+for i in range(len(a)):
+    if(s1[i]==l2[i]):
+        d.insert(a[i],s2[i])
+print(''.join(d))
